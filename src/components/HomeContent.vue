2b84dd67da66e18c66d119c9021bc5a7df4f0c86
Gostaug <pierrot_du34@hotmail.fr>
2021-02-25 10:09:33 +0100

    home component

---
+++ src/components/HomeContent.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="home-content">
+    <el-button type="primary" @click="goToForm">Not so nice form</el-button>
+    <TiltingBox/>
+  </div>
+</template>
+
+<script>
+import TiltingBox from './TiltingBox.vue'
+export default {
+  name: 'HomeContent',
+  components: {
+    TiltingBox
+  }
+  
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
---
+++ src/components/TiltingBox.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="tilting-box">
+        <el-button type="primary" @click="open">Skip</el-button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TiltingBox',
+  methods: {
+      open() {
+        this.$alert('This is a message', 'Title', {
+          confirmButtonText: 'OK',
+          callback: action => {
+            this.$message({
+              type: 'info',
+              message: `action: ${ action }`
+            });
+          }
+        });
+      }
+    }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
